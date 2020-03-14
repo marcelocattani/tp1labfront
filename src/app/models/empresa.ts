d37f72f8aa49c7cc361ff2e09ad7f86ca14df35a
Marcelo Cattani <marcelocattani96@gmail.com>
2020-03-14 15:31:08 -0300

    add:Models.empresa, models.noticia issue #2

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,11 @@
+export interface empresa{
+   id?:number;
+   denominacion:string;
+   telefono:string;
+   hoarioDeAtencion:string;
+   quienesSomos:string;
+   latitud:number;
+   longitud:number;
+   domicilio:string; 
+   email:string
+}
