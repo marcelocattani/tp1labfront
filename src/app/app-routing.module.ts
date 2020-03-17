e49a592c4f38440aa51b0fdf820b3abefd5de74b
Marcelo Cattani <marcelocattani96@gmail.com>
2020-03-17 18:30:10 -0300

    Add index #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: IndexComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
