e05646bae6ac6dc8c44b89ff82b2c58086497864
RamiroLopez47 <raami1999@gmail.com>
2020-03-18 14:21:09 -0300

    Agregación de componente y copia del Template detalle.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import {HttpClientModule} from '@angular/common/http';
 import { IndexComponent } from './components/index/index.component';
 import { Page404Component } from './components/page404/page404.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     Page404Component,
-    BuscadorComponent
+    BuscadorComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
