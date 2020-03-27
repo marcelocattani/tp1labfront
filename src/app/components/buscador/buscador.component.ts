4967d3cbbea43ce23543586cfdcc419946ffe637
Matias Ramos <matias.ramos@alumnos.frm.utn.edu.ar>
2020-03-27 19:24:00 -0300

    La logica esta completada, tuve que hacer unos cambios al backend para que funcione (el branch que use es pagina_buscador_backend). solo hace falta hacerlo ver como nos mostro el profesor Magni

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Noticia } from '../../models/noticia';
+import { Router } from '@angular/router';
+import { NoticiaService } from 'src/app/services/noticia.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router, private noticiaService: NoticiaService) { }
+
+  public textoBuscado = '';
+  public noticias: Noticia[];
 
   ngOnInit(): void {
   }
 
+
+  goNoticia(id: number) {
+    this.route.navigate(['/detalle/' + id]);
+  }
+
+  public buscar(formulario : NgForm){
+    console.info("Metodo no implementado")
+    console.info(formulario.value.textoBuscado);
+    this.textoBuscado=formulario.value.textoBuscado;
+    this.getNoticias();    
+  }
+
+  getNoticias() {
+    this.noticias = null;
+    this.noticiaService.buscar(this.textoBuscado).subscribe(dato => {
+      dato.forEach(n => {
+        n.resumenDeLaNoticia = n.resumenDeLaNoticia.substring(0, 40).concat('...');
+      });
+      dato.sort((a, b) => {
+        if (a.fechaPublicacion < b.fechaPublicacion) {
+          return 1;
+        }
+        if (a.fechaPublicacion > b.fechaPublicacion) {
+          return -1;
+        }
+        return 0;
+      });
+      this.noticias = dato;
+    },
+    err => {
+      alert('Ocurrió un error al cargar las Noticias: ' + err);
+    });
+  }
 }
