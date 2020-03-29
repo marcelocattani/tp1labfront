27ebe0b4160728642f46ddf843ce06833c5a0745
Juan <48636134+vargasjuanj@users.noreply.github.com>
2020-03-28 22:55:09 -0300

    Paginación abm-noticia componente. #23 (#34)
    * Aporte parcial. Componente tiny para crear y editar noticias #23.
    
    * Agregado componente abm-component, ruta ambcomponent. Falta relacionar la empresa con la noticia
    
    * Ruta abmnoticias
    
    * Paginacion
    
    Co-authored-by: vargasjuanj <juanjovargas4433377@gmail.com>

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -3,7 +3,6 @@ import { CommonService } from './common.service';
 import { Noticia } from '../models/noticia';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +12,16 @@ export class NoticiaService extends CommonService<Noticia> {
   constructor(http: HttpClient) {
     super(http);
     this.baseUrl = "http://localhost:9001/api/v1/noticia/";
+
   }
 
   public getNoticia(id: number): Observable<Noticia[]> {
-    return this.http.get<Noticia[]>(this.baseUrl + "recent?idEmpresa=" + id);
+    return this.http.get<Noticia[]>(this.baseUrl+ "recent?idEmpresa=" + id);
   }
 
+  public noticias(page: number, size: number, order: string, asc: boolean): Observable<any> {
+    return this.http.get<any>(this.baseUrl+'paginas?' + `page=${page}&size=${size}&order=${order}&asc=${asc}`);
+  }
   buscar(texto: string) {
     return this.http.get<Noticia[]>(this.baseUrl + 'search?word=' + texto);
   }
