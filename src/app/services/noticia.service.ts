638e9e462fdef0c619a8ce5e74064b48aa584859
Matias Ramos <matias.ramos@alumnos.frm.utn.edu.ar>
2020-03-28 21:00:46 -0300

    Cambio las direcciones a singluar como dice el issue #26 :grimacing:

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -12,7 +12,7 @@ export class NoticiaService extends CommonService<Noticia> {
 
   constructor(http: HttpClient) {
     super(http);
-    this.baseUrl = "http://localhost:9001/api/v1/noticias/";
+    this.baseUrl = "http://localhost:9001/api/v1/noticia/";
   }
 
   public getNoticia(id: number): Observable<Noticia[]> {
